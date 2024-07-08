73657fd8b933fc951d5fd38b8af21e770de1f46f
manlikexavy <chibokaxavier@gmail.com>
2024-07-08 16:05:30 +0100

    installed bcrypt and hashed password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,14 @@
 const User = require("../models/user.model");
+const bcryptjs = require("bcryptjs");
 const signUp = async (req, res) => {
   try {
     const { userName, email, password } = req.body;
-    const newUser = await User.create({ userName, email, password });
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = await User.create({
+      userName,
+      email,
+      password: hashedPassword,
+    });
     res.status(201).json({ newUser });
   } catch (error) {
     res.status(500).json({ error });
