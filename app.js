3a24dcac072cb08d446ce703b31533f734ce35c0
manlikexavy <chibokaxavier@gmail.com>
2024-07-08 11:37:45 +0100

    connected to db

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require("express");
-c
+const app = express();
+const mongoose = require("mongoose");
+const port = 3001;
+require("dotenv").config();
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    app.listen(port, console.log(`server listening on port ${port}`));
+  } catch (error) {
+    console.log(error);  
+  }
+};
+start();
